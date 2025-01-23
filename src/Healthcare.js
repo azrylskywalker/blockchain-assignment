b1f2ecd9e49cb6b39dee243a23ed786b52d8089d
Azryl <80805642+azrylskywalker@users.noreply.github.com>
2025-01-23 11:37:49 +0000

    final

--- src/Healthcare.js
+++ src/Healthcare.js
@@ -1,240 +1,270 @@
-import React, {useState, useEffect} from "react";
-import {ethers} from 'ethers'
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 const Healthcare = () => {
-    const [provider, setProvider] = useState(null);
-    const [signer, setSigner] = useState(null);
-    const [contract, setContract] = useState(null);
-    const [account, setAccount] = useState(null);
-    const [isOwner, setIsOwner] = useState(null);
-    const [patientID, setPatientID] = useState("");
-    const [diagnosis, setDiagnosis] = useState("");
-    const [treatment, setTreatment] = useState("");
-    const [patientRecords, setPatientRecords] = useState([]);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [isOwner, setIsOwner] = useState(null);
+  const [patientID, setPatientID] = useState("");
+  const [diagnosis, setDiagnosis] = useState("");
+  const [treatment, setTreatment] = useState("");
+  const [patientRecords, setPatientRecords] = useState([]);
 
-    const [providerAddress, setProviderAddress] = useState("");
+  const [providerAddress, setProviderAddress] = useState("");
+  const contractAddress = "0xe0c431d76285dee5519db0c22733dcd96640eb10";
 
-    //address from remix
-    const contractAddress = "0xE0c431d76285dEe5519DB0C22733dCD96640eb10";
-
-    //ABI from Remix
-    const contractABI = [
+  const contractABI = [
+    {
+      inputs: [
         {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "patientID",
-                    "type": "uint256"
-                },
-                {
-                    "internalType": "string",
-                    "name": "patientName",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "diagnosis",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "treatment",
-                    "type": "string"
-                }
-            ],
-            "name": "addRecord",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
+          internalType: "uint256",
+          name: "patientID",
+          type: "uint256",
         },
         {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "provider",
-                    "type": "address"
-                }
-            ],
-            "name": "authorizeProvider",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
+          internalType: "string",
+          name: "patientName",
+          type: "string",
         },
         {
-            "inputs": [],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
+          internalType: "string",
+          name: "diagnosis",
+          type: "string",
         },
         {
-            "inputs": [],
-            "name": "getOwner",
-            "outputs": [
-                {
-                    "internalType": "address",
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
+          internalType: "string",
+          name: "treatment",
+          type: "string",
         },
+      ],
+      name: "addRecord",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
         {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "patientID",
-                    "type": "uint256"
-                }
-            ],
-            "name": "getPatientRecords",
-            "outputs": [
-                {
-                    "components": [
-                        {
-                            "internalType": "uint256",
-                            "name": "recordID",
-                            "type": "uint256"
-                        },
-                        {
-                            "internalType": "string",
-                            "name": "patientName",
-                            "type": "string"
-                        },
-                        {
-                            "internalType": "string",
-                            "name": "diagnosis",
-                            "type": "string"
-                        },
-                        {
-                            "internalType": "string",
-                            "name": "treatment",
-                            "type": "string"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "timestamp",
-                            "type": "uint256"
-                        }
-                    ],
-                    "internalType": "struct HealthcareRecords.Record[]",
-                    "name": "",
-                    "type": "tuple[]"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        }
-    ];
+          internalType: "address",
+          name: "provider",
+          type: "address",
+        },
+      ],
+      name: "authorizeProvider",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+    {
+      inputs: [],
+      name: "getOwner",
+      outputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "patientID",
+          type: "uint256",
+        },
+      ],
+      name: "getPatientRecords",
+      outputs: [
+        {
+          components: [
+            {
+              internalType: "uint256",
+              name: "recordID",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "patientName",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "diagnosis",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "treatment",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "timestamp",
+              type: "uint256",
+            },
+          ],
+          internalType: "struct HealthcareRecords.Record[]",
+          name: "",
+          type: "tuple[]",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
 
-    useEffect(() => {
-        const connectWallet = async () => {
-            try{
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
-                await provider.send('eth_requestAccounts', []);
-                const signer = provider.getSigner();
+  useEffect(() => {
+    const connectWallet = async () => {
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        setProvider(provider);
+        setSigner(signer);
 
-                setProvider(provider);
-                setSigner(signer);
+        const accountAddress = await signer.getAddress();
+        setAccount(accountAddress);
 
-                const accountAddress = await signer.getAddress();
-                setAccount(accountAddress);
+        console.log(accountAddress);
 
-                const contract = new ethers.Contract(contractAddress, contractABI);
-                setContract(contract);
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        setContract(contract);
 
-                const ownerAddress  = await contract.getOwner();
+        const ownerAddress = await contract.getOwner();
 
-                setIsOwner(accountAddress.toLowerCase() === ownerAddress.toLowerCase())
+        setIsOwner(accountAddress.toLowerCase() === ownerAddress.toLowerCase());
+      } catch (error) {
+        console.error("Error connecting to wallet: ", error);
+      }
+    };
+    connectWallet();
+  }, []);
 
-            } catch (error) {
-                console.error("Error connecting to wallet: " , error);
-            }
-        };
-        connectWallet();
-
-    }, []);
-
-    const fetchPatientRecords = async () => {
-        try {
-            const records = await contract.getPatientRecords(patientID);
-            console.log(records);
-            setPatientRecords(records);
-        } catch (error) {
-           console.error("Error fetching patient records", error); 
-        }
+  const fetchPatientRecords = async () => {
+    try {
+      const records = await contract.getPatientRecords(patientID);
+      console.log(records);
+      setPatientRecords(records);
+    } catch (error) {
+      console.error("Error fetching patient records", error);
     }
+  };
 
-    const addRecord = async () => {
-        try {
-            const tx = await contract.addRecord (patientID, "placeholder", diagnosis, treatment);
-            await tx.wait();
-            fetchPatientRecords();
-            await tx.wait()
-        } catch (error) {
-            console.error("Error adding record", error);
-        }
+  const addRecord = async () => {
+    try {
+      const tx = await contract.addRecord(
+        patientID,
+        "Alice",
+        diagnosis,
+        treatment
+      );
+      await tx.wait();
+      fetchPatientRecords();
+      await tx.wait();
+      alert(`Record added`);
+    } catch (error) {
+      console.error("Error adding records", error);
     }
+  };
 
-    const authorizeProvider = async () => {
-        if (isOwner){
-            try {
-                const tx = await contract.authorizeProvider(providerAddress);
-                await tx.wait();
-                alert("Provider ${providerAddress} authorized successfully");
-    
-            } catch (error) {
-                console.error("Only contract owner can authorize different providers", error);
-            }
-        } else {
-            alert("Only contract owner can call this function.");
-        }
-        
+  const authorizeProvider = async () => {
+    if (isOwner) {
+      try {
+        const tx = await contract.authorizeProvider(providerAddress);
+        await tx.wait();
+        alert(`Provider ${providerAddress} authorized successfully`);
+      } catch (error) {
+        console.error("Only contract owner can authorize different providers");
+      }
+    } else {
+      alert("Only contract owner can call this function");
     }
+  };
 
-    return(
-        <div>
-            <div className="container">
-                <h1 className="title">Healthcare Application</h1>
-                {account && <p className="account-info">Connected Account:</p>}
-                {isOwner && <p className="owner-info">You are the contract owner</p>}
-            </div>
+  return (
+    <div className="container">
+      <h1 className="title">HealthCare Application</h1>
+      {account && <p className="account-info">Connected Account: {account}</p>}
+      {isOwner && <p className="owner-info">You are the contract owner</p>}
 
-            <div className="form-section">
-                <h2>Fetch Patient Records</h2>
-                <input className="input-field" type="text" placeholder="Enter Patient ID" value={patientID} onChange={(e) => setPatientID(e.target.value)}/>
-                <button className="action-button" onClick={fetchPatientRecords}>Fetch Record</button>
-            </div>
+      <div className="form-section">
+        <h2>Fetch Patient Records</h2>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Enter Patient ID"
+          value={patientID}
+          onChange={(e) => setPatientID(e.target.value)}
+        />
+        <button className="action-button" onClick={fetchPatientRecords}>
+          Fetch Records
+        </button>
+      </div>
 
-            <div className="form-section">
-                <h2>Add Patient Record</h2>
-                <input className="input-field" type="text" placeholder="Diagnosis" value={diagnosis} onChange={(e) => setDiagnosis(e.target.value)}/>
-                <input className="input-field" type="text" placeholder="Treatment" value={treatment} onChange={(e) => setTreatment(e.target.value)}/>
-                <button className="action-button" onClick={addRecord}>Add Record</button>
-            </div>
+      <div className="form-section">
+        <h2>Add Patient Record</h2>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Diagnosis"
+          value={diagnosis}
+          onChange={(e) => setDiagnosis(e.target.value)}
+        />
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Treatment"
+          value={treatment}
+          onChange={(e) => setTreatment(e.target.value)}
+        />
+        <button className="action-button" onClick={addRecord}>
+          Add Records
+        </button>
+      </div>
+      <div className="form-section">
+        <h2>Authorize HealthCare Provider</h2>
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Provider Address"
+          value={providerAddress}
+          onChange={(e) => setProviderAddress(e.target.value)}
+        />
+        <button className="action-button" onClick={authorizeProvider}>
+          Authorize Provider
+        </button>
+      </div>
 
-            <div className="form-section">
-                <h2>Authorize Healthcare Provider</h2>
-                <input className="input-field" type="text" placeholder="Provider Address" value = {providerAddress} onChange={(e) => setProviderAddress(e.target.value)}/>
-                <button className="action-button" onClick={authorizeProvider}>Authorize Provider</button>
-            </div>
+      <div className="records-section">
+        <h2>Patient Records</h2>
+        {patientRecords.map((record, index) => (
+          <div key={index}>
+            <p>Record ID: {record.recordID.toNumber()}</p>
+            <p>Diagnosis: {record.diagnosis}</p>
+            <p>Treatment: {record.treatment}</p>
+            <p>
+              Timestamp:{" "}
+              {new Date(record.timestamp.toNumber() * 1000).toLocaleString()}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            <div className="records-section">
-                <h2>Patient Records</h2>
-                {
-                    patientRecords.map((record, index) => (
-                        <div key = {index}>
-                            <p>Record ID: {record.recordID.toNumber()}</p>
-                            <p>Diagnosis: {record.diagnosis}</p>
-                            <p>Treatment: {record.treatment}</p>
-                            <p>Timestamp: {new Date(record.timestamp.toNumber() * 1000).toLocaleString()}</p>
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
-
-        
-    )
-}
-
-export default Healthcare
+export default Healthcare;
